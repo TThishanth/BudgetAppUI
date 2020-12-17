ce1d35c468172831f1e945839898f175e6fc8c46
Thishanth98 <thishanth41@gmail.com>
2020-12-17 11:05:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/BudgetAppUI/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.BudgetAppUI
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:BudgetAppUI/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Budget App UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
