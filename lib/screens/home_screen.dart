87669a6b426f2f5eb0912dded26317b493af0302
Thishanth98 <thishanth41@gmail.com>
2020-12-17 20:28:35 +0530

    Final Commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,9 @@
+import 'package:BudgetAppUI/data/data.dart';
+import 'package:BudgetAppUI/helpers/color_helper.dart';
+import 'package:BudgetAppUI/models/category_model.dart';
+import 'package:BudgetAppUI/models/expense_model.dart';
+import 'package:BudgetAppUI/screens/category_screen.dart';
+import 'package:BudgetAppUI/widgets/bar_chart.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,10 +12,150 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  _buildCategory(category, totalAmountSpent) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+           builder: (_) => CategoryScreen(category: category),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        padding: EdgeInsets.all(20.0),
+        height: 100.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  category.name,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                final double maxBarWidth = constraints.maxWidth;
+                final double percent = (category.maxAmount - totalAmountSpent) /
+                    category.maxAmount;
+                double barWidth = percent * maxBarWidth;
+                if (barWidth < 0) {
+                  barWidth = 0;
+                }
+                return Stack(
+                  children: [
+                    Container(
+                      height: 20.0,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                    Container(
+                      height: 20.0,
+                      width: barWidth,
+                      decoration: BoxDecoration(
+                        color: getColor(context, percent),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            forceElevated: true,
+            floating: true,
+            expandedHeight: 100.0,
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Budget App'),
+              centerTitle: true,
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                if (index == 0) {
+                  return Container(
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: BarChart(weeklySpending),
+                  );
+                } else {
+                  final Category category = categories[index - 1];
+                  double totalAmountSpent = 0;
+                  category.expenses.forEach((Expense expense) {
+                    totalAmountSpent += expense.cost;
+                  });
+                  return _buildCategory(category, totalAmountSpent);
+                }
+              },
+              childCount: 1 + categories.length,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
